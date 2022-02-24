c82ec461b03333a9dec6955aed2c16e345263fe2
James Gardner <james.gardner1421@gmail.com>
2022-02-24 11:18:50 +0000

    Set up testing

--- src/FortranNOAu111.jl
+++ src/FortranNOAu111.jl
@@ -5,7 +5,7 @@ export FortranNOAu111Model
 using NQCModels: NQCModels
 using Unitful, UnitfulAtomic
 using Libdl: Libdl
-using LinearAlgebra: Hermitian
+using LinearAlgebra: Hermitian, eigen
 
 struct FortranNOAu111Model <: NQCModels.DiabaticModels.LargeDiabaticModel
     energy_force_func::Ptr{Nothing}
@@ -20,6 +20,10 @@ struct FortranNOAu111Model <: NQCModels.DiabaticModels.LargeDiabaticModel
     r0::Matrix{Cdouble}
     mass::Vector{Cdouble}
     Ms::Cint
+    dA::Vector{Float64}
+    vA::Matrix{Float64}
+    DeltaE::Float64
+    nelectrons::Int
 end
 
 function FortranNOAu111Model(library_path, r; Ms, VAuFlag=1)
@@ -40,18 +44,23 @@ function FortranNOAu111Model(library_path, r; Ms, VAuFlag=1)
 
     a = sqrt(2) * dnn
     U = reshape([-1/sqrt(2),0,1/sqrt(2), 1/sqrt(2),-2/sqrt(6),1/sqrt(6), -1/sqrt(3),-1/sqrt(3),-1/sqrt(3) ], (3,3))
-    r0 = transpose(U) * reshape([0,1,1,0,1,-1,1,0,1,-1,0,1,1,1,0,1,-1,0,0,-1,-1,0,-1,1,-1,0,-1,1,0,-1,-1,-1,0,-1,1,0], (3,12)) ./ 2a
+    r0 = transpose(U) * reshape([0,1,1,0,1,-1,1,0,1,-1,0,1,1,1,0,1,-1,0,0,-1,-1,0,-1,1,-1,0,-1,1,0,-1,-1,-1,0,-1,1,0], (3,12)) ./ 2 .* a
 
     mass = zeros(N+2)
     mass[1] = ustrip(uconvert(u"kg", 14.00307440u"u"))
     mass[2] = ustrip(uconvert(u"kg", 15.99491502u"u"))
     mass[3:N+2] .= ustrip(uconvert(u"kg", 196.966548u"u"))
 
-    FortranNOAu111Model(energy_force_func, x, nn, N, dnn, aPBC, Hp, dHp, VAuFlag, r0, mass, Ms)
-end
+    DeltaE = austrip(7u"eV")
+    nelectrons = fld(Ms, 2)
+    eigs = eigen(get_Agauss(nelectrons))
+    dA = eigs.values
+    vA = eigs.vectors
 
-NQCModels.ndofs(::FortranNOAu111Model) = 3
-NQCModels.nstates(model::FortranNOAu111Model) = model.Ms+1
+    FortranNOAu111Model(energy_force_func, x, nn, N, dnn, aPBC, Hp, dHp, VAuFlag, r0, mass, Ms,
+        dA, vA, DeltaE, nelectrons
+    )
+end
 
 function get_nearest_neighbours(lib, N, r, dnn, aPBC)
     nn_func = Libdl.dlsym(lib, :get_nn)
@@ -64,16 +73,96 @@ function get_nearest_neighbours(lib, N, r, dnn, aPBC)
     return nn
 end
 
+NQCModels.ndofs(::FortranNOAu111Model) = 3
+NQCModels.nstates(model::FortranNOAu111Model) = model.Ms+1
+
+convert_energy(x) = austrip(x * u"J")
+convert_force(x) = austrip(x * u"J/m")
+
+get_neutral_element(model::FortranNOAu111Model) = convert_energy(model.Hp[1])
+get_ion_element(model::FortranNOAu111Model) = convert_energy(model.Hp[2])
+get_coupling_element(model::FortranNOAu111Model) = convert_energy(model.Hp[3])
+
+get_neutral_force(model::FortranNOAu111Model) = @views convert_force.(reshape(model.dHp[1,:], 3, :))
+get_ion_force(model::FortranNOAu111Model) = @views convert_force.(reshape(model.dHp[2,:], 3, :))
+get_coupling_force(model::FortranNOAu111Model) = @views convert_force.(reshape(model.dHp[3,:], 3, :))
+
 function set_coordinates!(model::FortranNOAu111Model, r)
     model.x .= ustrip.(auconvert.(u"m", r))
 end
 
+function NQCModels.state_independent_potential(model::FortranNOAu111Model, r::AbstractMatrix)
+    evaluate_energy_force_func!(model, r)
+    return get_neutral_element(model)
+end
+
+function NQCModels.state_independent_derivative!(model::FortranNOAu111Model, D::AbstractMatrix, r::AbstractMatrix)
+    evaluate_energy_force_func!(model, r)
+    D .= get_neutral_force(model)
+    return D
+end
+
 function NQCModels.potential!(model::FortranNOAu111Model, V::Hermitian, r::AbstractMatrix)
     evaluate_energy_force_func!(model, r)
+
+    burkey_cantrell_continuum!(model, V)
+    V[1,1] = get_ion_element(model) - get_neutral_element(model)
+
+    return V
 end
 
 function NQCModels.derivative!(model::FortranNOAu111Model, D::AbstractMatrix{<:Hermitian}, r::AbstractMatrix)
     evaluate_energy_force_func!(model, r)
+
+    for I in eachindex(D)
+        burkey_cantrell_continuum_derivative!(model, D, I)
+        D[I][1,1] = get_ion_force(model)[I] - get_neutral_force(model)[I]
+    end
+
+    return D
+end
+
+function get_Agauss(nelectrons)
+    mtemp = zeros(nelectrons,nelectrons)
+    for i in range(1,nelectrons-1)
+        mtemp[i,i+1] = i/sqrt((2i+1) * (2i-1))
+    end
+    mtemp = Hermitian(mtemp)
+    return mtemp
+end
+
+function burkey_cantrell_continuum!(model::FortranNOAu111Model, V::Hermitian)
+
+    (;nelectrons, dA, vA, DeltaE) = model
+    for i in range(2, nelectrons+1) # Set diagonal entries
+        V.data[i,i] =  (DeltaE/4.0)*dA[i-1] - DeltaE/4.0
+        V.data[i+nelectrons,i+nelectrons] =  (DeltaE/4.0)*dA[i-1] + DeltaE/4.0
+    end
+
+    E_coup = get_coupling_element(model)
+    vtemp = E_coup*sqrt.((DeltaE*2*vA[1,:].*vA[1,:])/4)./sqrt(DeltaE)
+
+    V.data[1,2:nelectrons+1] .= vtemp
+    V.data[2:nelectrons+1,1] .= vtemp
+    V.data[1,2+nelectrons:end] .= vtemp
+    V.data[2+nelectrons:end,1] .= vtemp
+end
+
+function burkey_cantrell_continuum_derivative!(model::FortranNOAu111Model, derivative::AbstractMatrix, I)
+
+    (;nelectrons, vA, DeltaE) = model
+    FF = get_coupling_force(model)[I]
+    # The expression here is without the division by the square root,
+    # bcs the square-root comes into the energy expression from the integration
+    # of the Schroedinger equation. The forces are not subject to Schroedinger.
+    # James: I'm using the sqrt to be consistent with the energy. Maybe we don't need is here or in the energy?
+    dvtemp =FF*sqrt.((DeltaE*2*vA[1,:].*vA[1,:])/4.0)./sqrt(DeltaE)
+    # dvtemp = FF*sqrt.((DeltaE*2*vA[1,:].*vA[1,:])/4.0)
+
+    derivative[I].data[1,2:nelectrons+1] .= dvtemp
+    derivative[I].data[2:nelectrons+1,1] .= dvtemp
+    derivative[I].data[1,2+nelectrons:end] .= dvtemp
+    derivative[I].data[2+nelectrons:end,1] .= dvtemp
 end
 
 function evaluate_energy_force_func!(model::FortranNOAu111Model, r::AbstractMatrix)
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,21 @@ using Test
 using DelimitedFiles
 using Unitful, UnitfulAtomic
 using FiniteDiff
+using LinearAlgebra
+
+function finite_difference_gradient(model, R)
+    f(x, j, i) = potential(model, x)[j,i]
+    grad = [Hermitian(zeros(nstates(model), nstates(model))) for _ in CartesianIndices(R)]
+    for i=1:nstates(model)
+        for j=1:nstates(model)
+            gradient = FiniteDiff.finite_difference_gradient(x->f(x,j,i), R)
+            for k in eachindex(R)
+                grad[k].data[j,i] = gradient[k]
+            end
+        end
+    end
+    grad
+end
 
 r = permutedims(readdlm("$(@__DIR__)/surface_Au111.dat"; skipstart=4))
 no = [
@@ -11,21 +26,49 @@ no = [
     0.0 0.0;
     2.0 2.0;
 ]
+r = austrip.(hcat(no, r) .* u"Å")
 
-model = FortranNOAu111Model("$(@__DIR__)/tullynoau111.dylib", r; Ms=5, VAuFlag=0)
-potential(model, r)
+model = FortranNOAu111Model("$(@__DIR__)/tullynoau111", r; Ms=6)
 
-function V_neutral(x)
-    potential(model, x)
-    e = model.Hp[1]
-    return austrip(e * u"J")
+@time @testset "Finite difference gradient for individual elements" begin
+    function V_neutral(x)
+        FortranNOAu111.evaluate_energy_force_func!(model, x)
+        return FortranNOAu111.get_neutral_element(model)
+    end
+
+    function V_ion(x)
+        FortranNOAu111.evaluate_energy_force_func!(model, x)
+        return FortranNOAu111.get_ion_element(model)
+    end
+
+    function V_coupling(x)
+        FortranNOAu111.evaluate_energy_force_func!(model, x)
+        return FortranNOAu111.get_coupling_element(model)
+    end
+
+    F1 = FiniteDiff.finite_difference_gradient(V_neutral, r)
+    F2 = FiniteDiff.finite_difference_gradient(V_ion, r)
+    F3 = FiniteDiff.finite_difference_gradient(V_coupling, r)
+
+    FortranNOAu111.evaluate_energy_force_func!(model, r)
+    analytic_neutral = FortranNOAu111.get_neutral_force(model)
+    analytic_ion = FortranNOAu111.get_ion_force(model)
+    analytic_coupling = FortranNOAu111.get_coupling_force(model)
+    @test F1 ≈ analytic_neutral 
+    @test F2 ≈ analytic_ion
+    @test F3 ≈ analytic_coupling
 end
 
-F = FiniteDiff.finite_difference_gradient(V_neutral, r)
-@info "Finite difference"
-display(F)
+@time @testset "Finite difference state independent" begin
+    V(x) = NQCModels.state_independent_potential(model, x)
+    F = FiniteDiff.finite_difference_gradient(V, r)
+    D = zero(r)
+    NQCModels.state_independent_derivative!(model, D, r)
+    @test D ≈ F
+end
 
-potential(model, r)
-F2 = austrip.(reshape(model.dHp[1,:], 3, :) .* u"J/m")
-
-F ./ F2
+@time @testset "Finite difference state dependent" begin
+    finite_diff = finite_difference_gradient(model, r)
+    analytic = derivative(model, r)
+    @test finite_diff ≈ analytic
+end
